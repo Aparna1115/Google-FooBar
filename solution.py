b8c4d1a4ae3c06346bbca2c58c12765ee341b1e3
Aparna1115 <37034467+Aparna1115@users.noreply.github.com>
2018-03-03 22:25:39 -0800

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,21 @@
+import math
+
+
+def answer(area):
+  
+	list=[]
+  diff=area
+  
+	while (diff!=0) : 
+	  
+		n=math.sqrt(area)
+	  
+		nFloor = math.floor(n)
+	  
+		diff = area - (nFloor*nFloor)
+	  
+		list.append(nFloor*nFloor)
+	  
+		area=diff
+  
+	return list
